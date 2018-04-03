d9263279c8639d8b42665317eb0186fb4b4c8f9d
Javier Rosales <javier.rb_96@hotmail.com>
2018-04-03 21:32:07 +0200

    RestService completed

--- app/src/main/java/com/javier/edukka/controllers/RetrofitClient.java
+++ app/src/main/java/com/javier/edukka/controllers/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    public static final String BASE_URL = "http://192.168.1.66/edukka/"; //https://edukka.000webhostapp.com/
+    public static final String BASE_URL = "http://192.168.1.66/edukka/"; //"https://edukka.000webhostapp.com/"
     public static RestService RETROFIT_CLIENT;
 
     public static RestService getInstance() {
